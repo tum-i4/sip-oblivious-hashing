490102f057ee884376ebee82d7ae4e9f7b1e123b
anahitH <anahithayrapetyan91@gmail.com>
2017-06-26 20:38:27 +0200

    Changed library name in run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 clang++-3.9 -std=c++0x ../bubble_sort.cpp -c -emit-llvm
-opt-3.9 -load /usr/local/lib/libInputDependency.so -load lib/libskeleton.so bubble_sort.bc -oh-insert -num-hash 1 -o out.bc
+opt-3.9 -load /usr/local/lib/libInputDependency.so -load lib/liboblivious-hashing.so bubble_sort.bc -oh-insert -num-hash 1 -o out.bc
 llvm-link-3.9 out.bc ../hashes/hash.bc -o out.bc
 llvm-link-3.9 out.bc ../assertions/asserts.bc -o out.bc
 
@@ -8,6 +8,6 @@ clang++-3.9 -rdynamic -std=c++0x -stdlib=libc++ out.bc -o out
 ./out 3
 rm out
 
-opt-3.9 -load /usr/local/lib/libInputDependency.so -load ../build/lib/libskeleton.so out.bc -insert-asserts -o protected.bc
+opt-3.9 -load /usr/local/lib/libInputDependency.so -load ../build/lib/liboblivious-hashing.so out.bc -insert-asserts -o protected.bc
 clang++-3.9 -rdynamic -std=c++0x protected.bc -o protected
 
