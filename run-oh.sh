24e9d715fef036beeff460efd8caabbf0da5a7ba
anahitH <anahithayrapetyan91@gmail.com>
2017-07-10 19:10:49 +0000

    Modified run-oh pass to re-compile asserts.cpp and hash.c for every run Removed unnecessary -stdlib flag

--- run-oh.sh
+++ run-oh.sh
@@ -5,15 +5,24 @@ OH_LIB=./build/lib
 bitcode=$1
 input=$2
 
+# compiling external libraries to bitcodes
+clang++-3.9 $OH_LIB/assertions/asserts.cpp -std=c++0x -c -emit-llvm -o $OH_LIB/assertions/asserts.bc
+clang-3.9 $OH_LIB/hashes/hash.c -c -emit-llvm -o $OH_LIB/hashes/hash.bc
+
+# Running hash insertion pass
 opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load  $OH_LIB/liboblivious-hashing.so $1 -oh-insert -num-hash 1 -o out.bc
+# Linking with external libraries
 llvm-link-3.9 out.bc ./hashes/hash.bc -o out.bc
 llvm-link-3.9 out.bc ./assertions/asserts.bc -o out.bc
 
-# dumps hashes
-clang++-3.9 -lncurses -rdynamic -std=c++0x -stdlib=libc++ out.bc -o out
+# precompute hashes
+clang++-3.9 -lncurses -rdynamic -std=c++0x out.bc -o out
 ./out $input
 ###rm out
 #
+
+# Running assertion insertion pass
 opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $OH_LIB/liboblivious-hashing.so out.bc -insert-asserts -o protected.bc
-clang++-3.9 -lncurses -rdynamic -std=c++0x -stdlib=libc++ protected.bc -o protected
+# Compiling to final protected binary
+clang++-3.9 -lncurses -rdynamic -std=c++0x protected.bc -o protected
 
