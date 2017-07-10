f08ca8096e4bf9b8785792eb3df670d0a110a605
anahitH <anahithayrapetyan91@gmail.com>
2017-07-10 19:15:06 +0000

    Modified run script, to use absolute path for OH instead of relative paths

--- run-oh.sh
+++ run-oh.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 INPUT_DEP_PATH=/usr/local/lib/
-OH_LIB=./build/lib
+OH_PATH=/home/sip/protection/introspection-oblivious-hashing
+OH_LIB=$OH_PATH/build/lib
 bitcode=$1
 input=$2
 
 # compiling external libraries to bitcodes
-clang++-3.9 $OH_LIB/assertions/asserts.cpp -std=c++0x -c -emit-llvm -o $OH_LIB/assertions/asserts.bc
-clang-3.9 $OH_LIB/hashes/hash.c -c -emit-llvm -o $OH_LIB/hashes/hash.bc
+clang++-3.9 $OH_PATH/assertions/asserts.cpp -std=c++0x -c -emit-llvm -o $OH_PATH/assertions/asserts.bc
+clang-3.9 $OH_PATH/hashes/hash.c -c -emit-llvm -o $OH_PATH/hashes/hash.bc
 
 # Running hash insertion pass
 opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load  $OH_LIB/liboblivious-hashing.so $1 -oh-insert -num-hash 1 -o out.bc
 # Linking with external libraries
-llvm-link-3.9 out.bc ./hashes/hash.bc -o out.bc
-llvm-link-3.9 out.bc ./assertions/asserts.bc -o out.bc
+llvm-link-3.9 out.bc $OH_PATH/hashes/hash.bc -o out.bc
+llvm-link-3.9 out.bc $OH_PATH/assertions/asserts.bc -o out.bc
 
 # precompute hashes
 clang++-3.9 -lncurses -rdynamic -std=c++0x out.bc -o out
