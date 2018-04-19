e400788bc9b3a479d391bd8267ec00863f4b160a
anahitH <anahithayrapetyan91@gmail.com>
2018-04-19 13:26:27 +0200

    Added simple source file for testing short-range oh

--- tests/test_cfgs.c
+++ tests/test_cfgs.c
@@ -1,81 +1,43 @@
 #include <stdio.h>
 
-//extern void assert(long long* hash, long long expected);
-//extern void hash1(long long *hashVariable, long long value);
+int g = 0;
 
-void test1(int i)
-{
-    printf("test1\n");
-    int res = 0;
-    //long long hash = 0;
-    //hash1(&hash, res);
-    if (i > 0) {
-        res = 1;
-        //hash1(&hash, res);
-        //assert(&hash, 7000000000000);
-        printf("positive\n");
-    }
-    res = -1;
-    printf("end test1\n");
-}
-
-void test2(int i)
+// entry -> if.then - can process as nothing is hashed in if.then
+// entry -> if.else can not process as k is used and is data indep
+// entry -> if.end can process as no data indep 
+void test2(int i, int j)
 {
     int k = 0;
+    k = j;
     if (i > 0) {
         k = 12;
-        printf("positive test2\n");
     } else {
         ++k;
-        printf("negative test2\n");
     }
     int res = k;
     printf("end test2\n");
 }
 
-/*
-void test3(int i)
+int test1(int i, int j)
 {
-    if (i > 0) {
-        if (i > 42) {
-            print("positive 42");
-        } else {
-            print("positive non 42");
-        }
-        print("positive");
+    int k = 32;
+    int n = 0;
+    if (k) {
+        --n;
     }
-    print("end");
+    return n;
 }
 
-void test4(int* n)
-{
-    int sum = 0;
-    for (int i = 0; i < *n; ++i) {
-        sum += i;
-    }
-}
-
-void test5(int n)
-{
-    int sum = 0;
-    int N = 42;
-    for (int i = 0; i < N; ++i) {
-        if (i < n) {
-            --sum;
-        }
-        ++sum;
-    }
-    test4(&sum);
-}
-*/
+// entry -> if.then - can process
+// entry -> if.end - can process
 int main(int argc, char* argv[])
 {
     printf("main\n");
-    test2(argc);
     int result = 0;
+    test2(argc, 0);
     if (argc > 2) {
+        test1(21, 22);
         printf("argc branch\n");
-        test1(argc);
         result = 12;
     }
     return 0;
