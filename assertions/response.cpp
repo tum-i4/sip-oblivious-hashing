fbeb959371b80ccd8701439c5462701cdbb86dd9
anahitH <anahithayrapetyan91@gmail.com>
2018-06-13 20:15:15 +0200

    Issue #109 - implemented assert to check if current placeholder has been seen before

---
+++ assertions/response.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <execinfo.h>
+#include <math.h>
+
+#include <unordered_set>
+#define DEBUG 1
+#define DEBUG2 0
+
+
+bool already_seen_placeholder(long long expected)
+{
+    static std::unordered_set<long long> placeholders;
+    return !placeholders.insert(expected).second;
+}
+
+extern "C" {
+
+void response() {
+	printf("Response mechanism.\n");
+//	exit(1);
+}
+
+void do_assert(long long* hash, long long expected)
+{
+	//if(DEBUG)
+	if(*hash != expected){
+        printf("\tAssert: %lld==%lld\n", *hash, expected);
+		void* callstack[128];
+		int i, frames = backtrace(callstack, 128);
+		char** strs = backtrace_symbols(callstack, frames);
+	
+
+		for (i = 0; i < frames; ++i) {
+			printf("%s\n", strs[i]);
+		}
+
+		free(strs);
+
+        //        printf("\tAssert: %lld==%lld\n", *hash, expected);
+		response();
+	}/*else {
+		//print the last functioni before assert in the trace
+		printf("%s\n",strs[1]);
+	}*/
+}
+
+void assert(long long* hash, long long expected) {
+    if (already_seen_placeholder(expected)) {
+        return;
+    }
+    do_assert(hash, expected);
+}
+
+void hash1(long long *hashVariable, long long value) {
+	*hashVariable = *hashVariable + value;
+	if(DEBUG2) printf("[h1] Hash=%lld, Value=%lld\n", *hashVariable, value);
+}
+
+void hash2(long long *hashVariable, long long value) {
+	*hashVariable = *hashVariable ^ value;
+	if(DEBUG2) printf("[h2] Hash=%lld, Value=%lld\n", *hashVariable, value);
+}
+
+}
