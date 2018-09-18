10e4c43d52a6552874846501c511435794434c48
anahitH <anahithayrapetyan91@gmail.com>
2018-09-18 14:07:27 +0200

    Updated run-oh-binary script Updated gdb scripts with dynamic library paths Updated README
    Re-added docker

--- README.md
+++ README.md
@@ -8,10 +8,20 @@
 	make
     
 # Running OH
-    Run run-oh-binary.sh script passing bitcode to protected.
+Run run-oh-binary.sh script passing bitcode to protected and optionally command line arguments for the protected binary. 
+For example to run protection on bubble_sort from tests run
 
-# Necessary tools to run OH
+	./run-oh-binary.sh bubble_sort.bc " 4"
+	
+where 4 is input to bubble_sort binary. 
+run-oh-binary.sh script will apply both OH and Short Range OH protection on full bitcode, i.e. all functions will be considered sensitive and protected. The output of this script is protected binary named ```out_patched```, it also creates oh and and input dependency analysis statistics files - ```oh.stats``` and ```dependency.stats``` respectively. 
+To run protected binary ```assertions/librtlib.so``` library should be preloaded with ```LD_PRELOAD```.    
+
+# Necessary tools to build and run OH
+In order to compile and use OH tool following tools need to be compiled and installed in your system.
+Refer to each of those projects for compilation and installing.
+
     Input dependency Analizer - https://github.com/mr-ma/input-dependency-analyzer
     DG - https://github.com/tum-i22/dg
+    self checksumming - https://github.com/mr-ma/self-checksumming
 
- Refer to each of those projects for compilation and installing.
--- run-oh-binary.sh
+++ run-oh-binary.sh
@@ -19,27 +19,10 @@ INPUT_DEP_PATH=/usr/local/lib/
 OH_PATH=/home/sip/sip-oblivious-hashing
 OH_LIB=$OH_PATH/build/lib
 bitcode=$1
-assert_list=$2
-input=$3
+input=$2
 
-# compiling external libraries to bitcodes
-#clang++-3.9 $OH_PATH/assertions/asserts.cpp -fno-use-cxa-atexit -std=c++0x -c -emit-llvm -o $OH_PATH/assertions/asserts.bc
-#clang-3.9 $OH_PATH/hashes/hash.c -c -fno-use-cxa-atexit -emit-llvm -o $OH_PATH/hashes/hash.bc
-#clang++-3.9 $OH_PATH/assertions/logs.cpp -fno-use-cxa-atexit -std=c++0x -c -emit-llvm -o $OH_PATH/assertions/logs.bc
-clang-6.0 $OH_PATH/assertions/response.c -c -fno-use-cxa-atexit -emit-llvm -o $OH_PATH/assertions/response.bc
- 
 # Running hash insertion pass
-#if [ $# -eq 2 ] 
-#  then
-#    echo "Assert file list is supplied"
-#    opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $UTILS_PATH -load  $OH_LIB/liboblivious-hashing.so $bitcode -oh-insert -num-hash 1 -skip 'hash' -dump-oh-stat="oh.stats" -assert-functions $assert_list -o out.bc
-#else
-    echo "No assert file is supplied.."
-    #echo "opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $UTILS_PATH -load $OH_LIB/liboblivious-hashing.so $bitcode -oh-insert -num-hash 1 -skip 'hash' -dump-oh-stat="oh.stats" -o out.bc"
-    #exit
-    opt-6.0 -load $INPUT_DEP_PATH/libInputDependency.so -load /usr/local/lib/libLLVMdg.so -load $UTILS_PATH -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -dependency-stats -dependency-stats-file='dependency.stats' -lib-config=/home/sip/input-dependency-analyzer/library_configs/tetris_library_config.json  -oh-insert -short-range-oh -num-hash 1 -skip 'hash' -dump-oh-stat="oh.stats" -o out.bc
-
-#fi
+    opt-6.0 -load $INPUT_DEP_PATH/libInputDependency.so -load /usr/local/lib/libLLVMdg.so -load $UTILS_PATH -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -goto-unsafe -dependency-stats -dependency-stats-file='dependency.stats' -lib-config=/home/sip/input-dependency-analyzer/library_configs/tetris_library_config.json  -oh-insert -short-range-oh -protect-data-dep-loops -num-hash 1 -skip 'hash' -dump-oh-stat="oh.stats" -o out.bc
 
 if [ $? -eq 0 ]; then
             echo 'OK Transform'
@@ -48,11 +31,17 @@ else
             exit    
 fi 
 
-llc-6.0 out.bc
-gcc -c -rdynamic out.s -o out.o
-gcc -g -rdynamic -c $OH_PATH/assertions/response.c -o response.o
-gcc -g -rdynamic out.o response.o -o out
+LIB_FILE=()
 
+llc out.bc
+g++ -std=c++0x -c -rdynamic out.s -o out.o
+
+g++ -fPIC -std=c++11 -g -rdynamic -c ${OH_PATH}/assertions/response.cpp -o ${OH_PATH}/assertions/oh_rtlib.o
+LIB_FILES+=( "${OH_PATH}/assertions/oh_rtlib.o" )
+
+g++ -std=c++11 -g -rdynamic -Wall -fPIC -shared -Wl,-soname,${OH_PATH}/assertions/libsrtlib.so -o "${OH_PATH}/assertions/librtlib.so" -lncurses -lm -lssl -lcrypto -pthread ${LIB_FILES[@]}
+
+g++ -std=c++11 -g -rdynamic out.o -o out -L${OH_PATH}/assertions/ -lrtlib -lncurses -lm -lssl -lcrypto -pthread
 
 # Linking with external libraries
 #llvm-link-3.9 out.bc $OH_PATH/assertions/response.bc -o out.bc
@@ -60,9 +49,13 @@ gcc -g -rdynamic out.o response.o -o out
 # intermediate precompute hashes
 #clang++-3.9 -g -lncurses -rdynamic -std=c++0x out.bc -o out
 
-#export LD_PRELOAD="/home/sip/self-checksumming/hook/build/libminm.so" 
+#
+##Patch using GDB
 
-#Patch using GDB
-python $OH_PATH/patcher/patchAsserts.py -b out -n out_patched -s oh.stats 
+export LD_PRELOAD="/home/sip/self-checksumming/hook/build/libminm.so ${OH_PATH}/assertions/librtlib.so"
+
+cmd_args=" ${input}"
+python $OH_PATH/patcher/patchAsserts.py -b out -n out_patched -s oh.stats -d False -g "$cmd_args" -p "/home/sip/sip-oblivious-hashing/assertions/gdb_script_for_do_assert.txt"
 echo 'Generated bianry is out_patched ...'
 chmod +x out_patched
+
